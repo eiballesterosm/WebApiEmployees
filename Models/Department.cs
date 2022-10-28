1b78a748f7c32df6ae2286921113b93c4e2d575c
eiballesterosm <eduballesteros@gmail.com>
2022-10-28 00:27:40 -0500

    Add the model classes Department and Employee. Add the DepartmentController and EmployeeController Add the connectionstring

---
+++ Models/Department.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApiEmployees.Models
+{
+    public class Department
+    {
+        public long DepartmentID { get; set; }
+        public string DepartmentName { get; set; }
+    }
+}
